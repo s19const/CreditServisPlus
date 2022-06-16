943240cacd3180c2f59ae816d5dffe1bf1be7ab4
const-test <kvshvarov@yandex.ru>
2022-06-16 15:34:18 +0300

    Initial commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,17 @@
 
-    public class CreditPaymentService {
-        public double calculate(int amount, int time) {
-            double percent = 9.9;
-            double rate = 1 + ((percent + 1) * (time - 1));
-            double payment = amount / rate;
+
+public class CreditPaymentService {
+
+    public double calculate(int amount, int time) {
+        double result = 1;
+        double percent = 9.9 / time * 100;                   // Ставка по кредиту в месяц
+        result = percent;                                          //        Цикл
+        for (int i = 1; i <= time; i++) {                    // возведение в степень
+            result = result * percent;                       //      12, 24, 36
+        }
+            double rate = percent * result / result - 1;     // коэффициент аннуитета
+            double payment = amount * rate;                  // ежемесячный платеж
+
             return payment;
         }
     }
