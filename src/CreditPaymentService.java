a085668912c6af567eb1b54070e77b7607f93f22
s19const <105714225+s19const@users.noreply.github.com>
2022-06-19 20:03:56 +0300

    Update CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,19 +1,15 @@
 
 
 public class CreditPaymentService {
-
     public double calculate(int amount, int time) {
 
-
-        double percent = 9.99 / (time * 100);                           // Ставка по кредиту в месяц
+        double percent = 9.99 / (time * 100);                         // Ставка по кредиту в месяц
         double result = percent + 1;                                  // Ставка по кредиту в месяц + 1
         float part = (float) Math.pow(result, time);                  // Возведение в степень
-
         double rate1 = percent * part;
         double rate2 = part - 1;
-        double rate = rate1 / rate2;                                 // коэффициент аннуитета
-        double payment = amount * rate;                              // ежемесячный платеж
-
+        double rate = rate1 / rate2;                                  // коэффициент аннуитета
+        double payment = amount * rate;                               // ежемесячный платеж
         return payment;
     }
 }
